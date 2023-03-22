f0e5b1052a5d0dd4291ff1e1d11d3a2a0e7468d6
khaliliali <ali.khalili@outlook.com>
2023-03-23 00:42:23 +0800

    Add Terraform Git and changes on current state and desire state

--- v001/aws_ec2.tf
+++ v001/aws_ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-northeast-1"
-  access_key = "###"
-  secret_key = "###"
+  access_key = "AKIA3GXE3DS36MV46KF4"
+  secret_key = "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"
 }
 
 resource "aws_instance" "terraform-ec2" {
